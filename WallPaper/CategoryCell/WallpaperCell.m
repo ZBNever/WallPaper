14c75b7a87a9240a49084e0a65c3c48d3005e61b
Never <never1750@163.com>
2021-06-05 20:29:42 +0800

    更新图片显示

--- WallPaper/CategoryCell/WallpaperCell.m
+++ WallPaper/CategoryCell/WallpaperCell.m
@@ -13,23 +13,24 @@
 #import "YYKit.h"
 
 @implementation WallpaperCell{
-    UIImageView *_image;
+    UIImageView *_imageView;
     YYAnimatedImageView *_webImageView;
 }
 
 - (instancetype)initWithFrame:(CGRect)frame{
     if (self = [super initWithFrame:frame]) {
-        _image = [[UIImageView alloc] init];
-        _image.contentMode = UIViewContentModeScaleAspectFill;
-        _image.clipsToBounds = YES;
-//        [self.contentView addSubview:_image];
+        _imageView = [[UIImageView alloc] init];
+        _imageView.contentMode = UIViewContentModeScaleAspectFill;
+        _imageView.clipsToBounds = YES;
+        _imageView.size = self.size;
+        [self.contentView addSubview:_imageView];
         
-        _webImageView = [YYAnimatedImageView new];
-        _webImageView.size = self.size;
-        _webImageView.clipsToBounds = YES;
-        _webImageView.contentMode = UIViewContentModeScaleAspectFill;
-//        _webImageView.backgroundColor = [UIColor whiteColor];
-        [self.contentView addSubview:_webImageView];
+//        _webImageView = [YYAnimatedImageView new];
+//        _webImageView.size = self.size;
+//        _webImageView.clipsToBounds = YES;
+//        _webImageView.contentMode = UIViewContentModeScaleAspectFill;
+////        _webImageView.backgroundColor = [UIColor whiteColor];
+//        [self.contentView addSubview:_webImageView];
         
     }
     return self;
@@ -37,28 +38,34 @@
 
 
 - (void)setPixabayModel:(PixabayModel *)model{
-
+    
+//    [_imageView sd_setImageWithURL:model.webformatURL completed:^(UIImage * _Nullable image, NSError * _Nullable error, SDImageCacheType cacheType, NSURL * _Nullable imageURL) {
+//
+//    }];
+    
+    
 //    WeakSelf
-    [_webImageView setImageWithURL:model.webformatURL
+    [_imageView setImageWithURL:model.webformatURL
                        placeholder:nil
                            options:YYWebImageOptionProgressiveBlur | YYWebImageOptionShowNetworkActivity | YYWebImageOptionSetImageWithFadeAnimation
                         completion:^(UIImage * _Nullable image, NSURL * _Nonnull url, YYWebImageFromType from, YYWebImageStage stage, NSError * _Nullable error) {
-        
+/**
         if (stage == YYWebImageStageFinished) {
-                
+
                 self->_webImageView.alpha = 0;
                 //旋转
                 self->_webImageView.transform = CGAffineTransformRotate(CGAffineTransformMakeScale(.5, .5), drand48()-0.5);
                 //缩放
                 self->_webImageView.transform = CGAffineTransformMakeScale(0.6, 0.6);
-                
+
                 [UIView animateWithDuration:1.0 delay:0 usingSpringWithDamping:0.8 initialSpringVelocity:0 options:0 animations:^{
-                    
+
                     self->_webImageView.transform = CGAffineTransformIdentity;
                     self->_webImageView.alpha = 1;
-                    
+
                 } completion:nil];
             }
+ */
     }
         ];
 
